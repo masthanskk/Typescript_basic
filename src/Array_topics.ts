97e3a6b2814b583bfa04d1011aaa4ac5258fd70a
Masthan <skmasthan.1999@gmail.com>
2025-07-19 21:44:11 +0530

    first change

--- src/Array_topics.ts
+++ src/Array_topics.ts
@@ -24,6 +24,7 @@
   // Ex: 
    let Citynum:string[] | number[] = [1,1,2,3,4,5,6]
          Citynum[0] = 0 // here it accepts 
+         
    
   // ShortHand Notation is nothing but taking array it should be complete String or Number data type thats the flow of shorthand Notation
   
